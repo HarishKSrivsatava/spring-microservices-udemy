b541baf8ad1e575e5d00faa80e317d0a2d4b95a5
Harish Kumar <harishkumar9758@gmail.com>
2021-07-24 18:14:46 +0530

    First commit

---
+++ spring-boot-microservices-udemy/src/main/java/com/harish/springboot/springbootmicroservicesudemy/SpringBootMicroservicesUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.harish.springboot.springbootmicroservicesudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicesUdemyApplication.class, args);
+	}
+
+}
---
+++ spring-boot-microservices-udemy/src/test/java/com/harish/springboot/springbootmicroservicesudemy/SpringBootMicroservicesUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harish.springboot.springbootmicroservicesudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
